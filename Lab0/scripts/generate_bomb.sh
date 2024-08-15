86cacb5e4e160ff0ec23649fda8afe1bae6927bc
Yiyang Wu <toolmanp@tlmp.cc>
2024-08-15 15:51:35 +0800

    lab0: refactor grade scripts
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- Lab0/scripts/generate_bomb.sh
+++ Lab0/scripts/generate_bomb.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ ! -d "./warehouse" ]; then
+if [ ! -d "${CURDIR}/warehouse" ]; then
     echo "Please run under the root directory of this lab!"
     exit -1
 fi
 
 total_bombs=30
-student=$(cat ./student-number.txt)
+student=$(cat ${CURDIR}/student-number.txt)
 
 if [ -z $student ]; then
     echo "Please enter your student number in student-number.txt at first! Otherwise you would recieve 0 POINTS!"
@@ -15,4 +15,4 @@ fi
 
 bomb_id=$(($student%$total_bombs+1))
 
-cp ./warehouse/bomb-${bomb_id} ./bomb
+cp ${CURDIR}/warehouse/bomb-${bomb_id} ${CURDIR}/bomb
