4123a688184584a2064e79131319cdcb446bbf51
Yiyang Wu <toolmanp@tlmp.cc>
2024-08-17 22:09:03 +0800

    labs: use better structured makefile
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- Scripts/lab.mk
+++ Scripts/lab.mk
@@ -2,12 +2,25 @@
 # This sets up the environment variable for lab's Makefile.
 
 ifndef PROJECT
-PROJECT:=(shell git rev-parse --show-toplevel)
+PROJECT := $(shell git rev-parse --show-toplevel)
 endif
 
 # Toolchain configuration
-GDB?=gdb
+GDB ?= gdb
 
 ifeq (,$(wildcard $(PROJECT)/Scripts/env_generated.mk))
-$(error Please run fixdeps to create the environment first!)
+	$(error Please run fixdeps to create the environment first!)
+endif
+
+ifeq (,$(LAB))
+$(error LAB is not set!)
+endif
+
+ifeq ($(shell test $(LAB) -eq 0; echo $$?),1)
+	ifeq ($(shell test $(LAB) -gt 5; echo $$?),0)
+		include $(PROJECT)/Scripts/extras.mk
+	else
+		include $(PROJECT)/Scripts/kernel.mk
+	endif
+	include $(PROJECT)/Scripts/submit.mk
 endif
