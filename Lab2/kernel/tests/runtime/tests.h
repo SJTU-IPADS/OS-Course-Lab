52f2d4c6aaa6836309bdfc3fd81fc8af6c9d6e2e
Yiyang Wu <toolmanp@tlmp.cc>
2024-09-21 14:11:35 +0800

    feat(lab2): use new tests architecture
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- Lab2/kernel/tests/runtime/tests.h
+++ Lab2/kernel/tests/runtime/tests.h
@@ -13,22 +13,20 @@
 #ifndef KERNEL_TESTS_RUNTIME_TESTS_H
 #define KERNEL_TESTS_RUNTIME_TESTS_H
 
-#define lab_check(expr, test_name)                                        \
-        do {                                                              \
-                if (expr) {                                               \
-                        printk("[TEST] %s: OK\n", test_name);             \
-                } else {                                                  \
-                        printk("[TEST] %s: FAIL, loc: %s:%d, expr: %s\n", \
-                               test_name,                                 \
-                               __FILE__,                                  \
-                               __LINE__,                                  \
-                               #expr);                                    \
-                }                                                         \
+#define TEST_SUITE(lab, name) void lab##_##name(void)
+#define TEST(name)                           \
+        for (const char *test_name = (name); \
+             printk("[TEST] %s: OK\n", test_name),0;)
+#define ASSERT(expr)                                                       \
+        do {                                                               \
+                if (!(expr))                                               \
+                                                                           \
+                {                                                          \
+                        printk("[TEST] %s: FAIL, loc: %s: %d, expr: %s\n", \
+                               test_name,                                  \
+                               __FILE__,                                   \
+                               __LINE__,                                   \
+                               #expr);                                     \
+                }                                                          \
         } while (0)
-
-#define lab_assert(expr)           \
-        do {                       \
-                ok = ok && (expr); \
-        } while (0)
-
 #endif /* KERNEL_TESTS_RUNTIME_TESTS_H */
