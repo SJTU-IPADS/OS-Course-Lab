1842893c3c5ab14f1354a084ef4cc2d19f6177f1
Yiyang Wu <toolmanp@tlmp.cc>
2024-09-30 00:37:51 +0800

    feat(lab3): update lab-defs
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- Lab3/filelist.mk
+++ Lab3/filelist.mk
@@ -9,4 +9,12 @@ CONTEXT := $(SCHED_ARCH)/context.c
 IRQ_ARCH := kernel/arch/aarch64/irq
 IRQ_ENTRY_S := $(IRQ_ARCH)/irq_entry.S
 
-FILES := $(CAP_GROUP) $(THREAD) $(CONTEXT) $(IRQ_ENTRY_S)
+STDIO := user/chcore-libc/libchcore/porting/overrides/src/chcore-port/stdio.c
+
+HELLO := ramdisk/hello_world.bin
+FILES := $(CAP_GROUP) \
+		 $(THREAD) \
+		 $(CONTEXT) \
+		 $(IRQ_ENTRY_S) \
+		 $(STDIO) \
+		 $(HELLO)
