562e6cf93772a7b98bedea27040ecf0082058595
Yiyang Wu <toolmanp@tlmp.cc>
2024-10-03 12:18:01 +0800

    fix(lab2): add missing kmalloc.c
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- Lab2/filelist.mk
+++ Lab2/filelist.mk
@@ -4,9 +4,10 @@ ARCH := kernel/arch/aarch64
 
 BUDDY := $(MM)/buddy.c
 SLAB := $(MM)/slab.c
+KMALLOC := $(MM)/kmalloc.c
 PTE := $(MM_ARCH)/page_table.c 
 MAIN := $(ARCH)/main.c
 PGFAULT := $(ARCH)/irq/pgfault.c $(MM)/pgfault_handler.c
 VMSPACE := $(MM)/vmspace.c
 
-FILES := $(BUDDY) $(SLAB) $(PTE) $(MAIN) $(PGFAULT) $(VMSPACE)
+FILES := $(BUDDY) $(KMALLOC) $(SLAB) $(PTE) $(MAIN) $(PGFAULT) $(VMSPACE)
