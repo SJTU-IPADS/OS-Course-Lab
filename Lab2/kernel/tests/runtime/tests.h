510f0e0c4ab9f120cee3292ab47fcba283b13902
Yiyang Wu <toolmanp@tlmp.cc>
2024-10-12 21:45:34 +0800

    fix(lab2): fix wrong test header
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- Lab2/kernel/tests/runtime/tests.h
+++ Lab2/kernel/tests/runtime/tests.h
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 2023 Institute of Parallel And Distributed Systems (IPADS), Shanghai Jiao Tong University (SJTU)
- * Licensed under the Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * Copyright (c) 2023 Institute of Parallel And Distributed Systems (IPADS),
+ * Shanghai Jiao Tong University (SJTU) Licensed under the Mulan PSL v2. You can
+ * use this software according to the terms and conditions of the Mulan PSL v2.
  * You may obtain a copy of Mulan PSL v2 at:
  *     http://license.coscl.org.cn/MulanPSL2
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
- * PURPOSE.
- * See the Mulan PSL v2 for more details.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE. See the
+ * Mulan PSL v2 for more details.
  */
 
 #ifndef KERNEL_TESTS_RUNTIME_TESTS_H
@@ -15,19 +15,20 @@
 
 extern char serial_number[4096];
 #define TEST_SUITE(lab, name) void lab##_##name(void)
-#define TEST(name)                           \
-        for (const char *test_name = (name); \
-             printk("[TEST] %s: OK: %s\n", test_name, serial_number),0;)
-#define ASSERT(expr)                                                       \
-        do {                                                               \
-                if (!(expr))                                               \
-                                                                           \
-                {                                                          \
-                        printk("[TEST] %s: FAIL, loc: %s: %d, expr: %s\n", \
-                               test_name,                                  \
-                               __FILE__,                                   \
-                               __LINE__,                                   \
-                               #expr);                                     \
-                }                                                          \
-        } while (0)
+#define TEST(name)                                                   \
+        for (char *i = (char *)(long)1, *test_name = (name); i != 0; \
+             i--, printk("[TEST] %s: OK: %s\n", test_name, serial_number))
+#define ASSERT(expr)                                               \
+        if (!(expr))                                               \
+                                                                   \
+        {                                                          \
+                printk("[TEST] %s: FAIL, loc: %s: %d, expr: %s\n", \
+                       test_name,                                  \
+                       __FILE__,                                   \
+                       __LINE__,                                   \
+                       #expr);                                     \
+                break;                                             \
+                while (1)                                          \
+                        ;                                          \
+        }
 #endif /* KERNEL_TESTS_RUNTIME_TESTS_H */
