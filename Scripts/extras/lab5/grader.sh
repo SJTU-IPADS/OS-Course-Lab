7de2bb8d9decc3d6b07ad00a0509e68945a87a38
Birdman0626 <166781837+Birdman0626@users.noreply.github.com>
2025-01-16 14:17:24 +0800

    Update grader.sh

--- Scripts/extras/lab5/grader.sh
+++ Scripts/extras/lab5/grader.sh
@@ -16,7 +16,9 @@ fi
 cp "${FSM_DIR}/CMakeLists.txt" "${FSM_DIR}/CMakeLists.txt.bak"
 cp "${FS_BASE_DIR}/CMakeLists.txt" "${FS_BASE_DIR}/CMakeLists.txt.bak"
 
-info "Grading lab ${LAB} ...(may take ${TIMEOUT} seconds)"
+read -r var1 var2 var3 var4 var5 <<< "${TIMEOUT}"
+info "Grading lab ${LAB} ...(may take $var1 seconds)"
+#info "Grading lab ${LAB} ...(may take ${TIMEOUT} seconds)"
 bold "==========================================="
 score=0
 # Part1 FSM
@@ -24,7 +26,9 @@ cp "${CMAKE_EXTRA_DIR}/cmake-fsm-full.txt" "${FSM_DIR}/CMakeLists.txt"
 cp "${CMAKE_EXTRA_DIR}/cmake-fs_base-part1.txt" "${FS_BASE_DIR}/CMakeLists.txt"
 make distclean &> /dev/null
 make build &> /dev/null
-${SCRIPTS}/capturer.py -f ${LABDIR}/scores-part1.json -t 30 make qemu-grade 2> /dev/null
+info "Grading part 1 ...(may take $var2 seconds)"
+${SCRIPTS}/capturer.py -f ${LABDIR}/scores-part1.json -t $var2 make qemu-grade 2> /dev/null
+#${SCRIPTS}/capturer.py -f ${LABDIR}/scores-part1.json -t 60 make qemu-grade 2> /dev/null
 score=$(($score+$?))
 if [[ $score -eq 255 ]]; then
 	error "Something went wrong. Please check the output of your program"
@@ -35,7 +39,9 @@ cp "${CMAKE_EXTRA_DIR}/cmake-fsm-part2.txt" "${FSM_DIR}/CMakeLists.txt"
 cp "${CMAKE_EXTRA_DIR}/cmake-fs_base-part2-vnode.txt" "${FS_BASE_DIR}/CMakeLists.txt"
 make distclean &> /dev/null
 make build &> /dev/null
-${SCRIPTS}/capturer.py -f ${LABDIR}/scores-part2.json -t 30 make qemu-grade 2> /dev/null
+info "Grading part 2 ...(may take $var3 seconds)"
+${SCRIPTS}/capturer.py -f ${LABDIR}/scores-part2.json -t $var3 make qemu-grade 2> /dev/null
+#${SCRIPTS}/capturer.py -f ${LABDIR}/scores-part2.json -t 60 make qemu-grade 2> /dev/null
 score=$(($score+$?))
 if [[ $score -eq 255 ]]; then
 	error "Something went wrong. Please check the output of your program"
@@ -48,7 +54,9 @@ cp "${CMAKE_EXTRA_DIR}/cmake-fsm-part2.txt" "${FSM_DIR}/CMakeLists.txt"
 cp "${CMAKE_EXTRA_DIR}/cmake-fs_base-part2-server_entry.txt" "${FS_BASE_DIR}/CMakeLists.txt"
 make distclean &> /dev/null
 make build &> /dev/null
-${SCRIPTS}/capturer.py -f ${LABDIR}/scores-part3.json -t 30 make qemu-grade 2> /dev/null
+info "Grading part 3 ...(may take $var4 seconds)"
+${SCRIPTS}/capturer.py -f ${LABDIR}/scores-part3.json -t $var4 make qemu-grade 2> /dev/null
+#${SCRIPTS}/capturer.py -f ${LABDIR}/scores-part3.json -t 60 make qemu-grade 2> /dev/null
 score=$(($score+$?))
 if [[ $score -eq 255 ]]; then
 	error "Something went wrong. Please check the output of your program"
@@ -60,7 +68,9 @@ mv "${FSM_DIR}/CMakeLists.txt.bak" "${FSM_DIR}/CMakeLists.txt"
 mv "${FS_BASE_DIR}/CMakeLists.txt.bak" "${FS_BASE_DIR}/CMakeLists.txt"
 make distclean &> /dev/null
 make build &> /dev/null
-${SCRIPTS}/capturer.py -f ${LABDIR}/scores-part4.json -t 30 make qemu-grade 2> /dev/null
+info "Grading part 4 ...(may take $var5 seconds)"
+${SCRIPTS}/capturer.py -f ${LABDIR}/scores-part4.json -t $var5 make qemu-grade 2> /dev/null
+#${SCRIPTS}/capturer.py -f ${LABDIR}/scores-part4.json -t 60 make qemu-grade 2> /dev/null
 score=$(($score+$?))
 if [[ $score -eq 255 ]]; then
 	error "Something went wrong. Please check the output of your program"
