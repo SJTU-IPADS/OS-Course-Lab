c148da587393368718bf56197451a151f82e4105
Kiwi <qiweic10@sjtu.edu.cn>
2025-02-21 10:02:51 +0800

    Update lab list

--- MallocLab/handout/fsecs.h
+++
@@ -1,4 +0,0 @@
-typedef void (*fsecs_test_funct)(void *);
-
-void init_fsecs(void);
-double fsecs(fsecs_test_funct f, void *argp);
--- MallocLab/handout/mm.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-extern int mm_init (void);
-extern void *mm_malloc (size_t size);
-extern void mm_free (void *ptr);
-extern void *mm_realloc(void *ptr, size_t size);
-
